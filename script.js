464669ff66c046b8fdd0bc179eaa06f516a9af1b
Mboutidem <mboutidemmkpong1@gmail.com>
2022-09-13 23:17:46 -0400

    Update all Files and UI for Gameplay

--- script.js
+++ script.js
@@ -6,64 +6,87 @@ function getComputerChoice () {
     return(computer_choice);
 }
 
+var div = document.getElementById('text-box')
+var user_score = document.getElementById('user-score');
+var comp_score = document.getElementById('iya-score');
+
 let winner=1; //computer wins
+let computerWin = 0, userWin = 0;
 function playRound (playerSelection,computerSelection) {
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
         //Draw, play again
-        alert("Draw! Play again!");
+        div.innerHTML = "Draw! Play again!";
         winner = 0; //Draw
     }
     else if ((playerSelection.toLowerCase() === "rock")&&(computerSelection.toLowerCase() === "paper")) {
         // computer wins
-        alert("You Lose! Paper beats Rock");
+        div.innerHTML="You Lose! Paper beats Rock";
+        winner=1;
     }
     else if ((playerSelection.toLowerCase() === "paper")&&(computerSelection.toLowerCase() === "rock")) {
         // player wins
-        alert("You Win! Paper beats Rock");
+        div.innerHTML="You Win! Paper beats Rock";
         winner = -1;
     }
     else if ((playerSelection.toLowerCase() === "rock")&&(computerSelection.toLowerCase() === "scissors")) {
         // player wins
-        alert("You Win! Rock beats Scissors");
+        div.innerHTML="You Win! Rock beats Scissors";
         winner = -1;
     }
     else if ((playerSelection.toLowerCase() === "scissors")&&(computerSelection.toLowerCase() === "rock")) {
         // computer wins
-        alert("You Lose! Rock beats Scissors");
+        div.innerHTML="You Lose! Rock beats Scissors";
+        winner=1;
     }
     else if ((playerSelection.toLowerCase() === "paper")&&(computerSelection.toLowerCase() === "scissors")) {
         // computer wins
-        alert("You Lose! Scissors beats Paper");
+        div.innerHTML="You Lose! Scissors beats Paper";
+        winner=1;
     }
     else if ((playerSelection.toLowerCase() === "scissors")&&(computerSelection.toLowerCase() === "paper")) {
        // player wins 
-       alert("You win! Scissors beats Paper");
+       div.innerHTML="You win! Scissors beats Paper";
        winner = -1;
     }
-}
 
-function game() {
-    let computerWin = 0, userWin = 0;
-    for (let i = 0; i < 5; i++) {
-        let userInput = prompt("Enter your Play [Rock,Paper,or Scissors]");
-        let round = playRound(userInput,getComputerChoice());
-        if (winner === 1){
-            computerWin+=1;
-        }
-        else if (winner === -1) {
-            userWin+=1;
-        }
+    if (winner === 1){ //Keep Round Winner Count
+        computerWin+=1;
+        comp_score.innerHTML=''+computerWin;
+    }
+    else if (winner === -1) {
+        userWin+=1;
+        user_score.innerHTML=''+userWin;
     }
 
-    if (computerWin > userWin) {
-        alert("You Lose! The Computers Won");
+
+    if (computerWin >= 5) { //Announce Winner After 5 wins
+        div.innerHTML="Iya Tao won. Prepare for a whooping";
+        window.confirm('Iya Tao won. Prepare for a whooping');
+        if (confirm("Play Again?")) {
+            location.reload();
+        }
     }
-    else if (userWin > computerWin) {
-        alert("You Won! You defeated the Computers.");
+    else if (userWin >= 5) {
+        div.innerHTML="You won! You defeated Iya Tao.";
+        window.confirm('You won! You defeated Iya Tao.');
+        if (confirm("Play Again?")) {
+            location.reload();
+        }
     }
-    else {
-        alert("You Drew with the Robots. Play again!");
-        game();
-    }
-}
+};
+
+const rock = document.getElementById("rock");
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+
+
+rock.addEventListener('click', function(){
+    playRound('rock',getComputerChoice());
+});
+paper.addEventListener('click', function(){
+    playRound('paper',getComputerChoice());
+});
+scissors.addEventListener('click', function(){
+    playRound('scissors',getComputerChoice());
+});
 
